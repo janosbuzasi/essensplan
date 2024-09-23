1f8a96aa6588f6d7ee8277b463df5177662f10b8
janosbuzasi <janos.buzasi@bluewin.ch>
2024-09-23 14:33:56 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="footer-content">
         <p>&copy; <?php echo date("Y"); ?> Essensplan by Janos Buzasi - Alle Rechte vorbehalten.</p>
         <ul class="social-links">
-            <li><a href="https://github.com/janosbuzasi/essensplan">github</a></li>
+            <li><a href="https://github.com/janosbuzasi/essensplan">_github</a></li>
         </ul>
     </div>
 </footer>
