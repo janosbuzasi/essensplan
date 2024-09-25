591a45cbc0620800f178b420208e02a7bcffbc71
janosbuzasi <janos.buzasi@bluewin.ch>
2024-09-25 18:53:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,11 +11,32 @@ $userLoggedIn = isset($_SESSION['username']);
     <?php if ($userLoggedIn): ?>
         <p>Hallo <?php echo $_SESSION['username']; ?>, willkommen zurück!</p>
         <p>Hier kannst du deine Essenspläne und Rezepte verwalten:</p>
-        <ul>
-            <li><a href="/essensplan/src/view_recipes.php" class="btn btn-view"><i class="fas fa-utensils"></i> Rezepte verwalten</a></li>
-            <li><a href="/essensplan/src/view_weeks.php" class="btn btn-view"><i class="fas fa-calendar-alt"></i> Wochenpläne verwalten</a></li>
-            <li><a href="/essensplan/src/archived_weeks.php" class="btn btn-view"><i class="fas fa-archive"></i> Archivierte Essenspläne</a></li>
-        </ul>
+        <div class="icon-menu">
+            <a href="/essensplan/index.php" class="icon-menu-item" title="Home">
+                <i class="fas fa-home"></i>
+                <span>Home</span>
+            </a>
+            <a href="/essensplan/src/view_recipes.php" class="icon-menu-item" title="Rezepte verwalten">
+                <i class="fas fa-utensils"></i>
+                <span>Rezepte verwalten</span>
+            </a>
+            <a href="/essensplan/src/view_categories.php" class="icon-menu-item" title="Mahlzeitenkategorien verwalten">
+                <i class="fas fa-list"></i>
+                <span>Mahlzeitenkategorien</span>
+            </a>
+            <a href="/essensplan/src/view_weeks.php" class="icon-menu-item" title="Wochenpläne verwalten">
+                <i class="fas fa-calendar-alt"></i>
+                <span>Wochenpläne</span>
+            </a>
+            <a href="/essensplan/src/archived_weeks.php" class="icon-menu-item" title="Archivierte Essenspläne">
+                <i class="fas fa-archive"></i>
+                <span>Archivierte Pläne</span>
+            </a>
+            <a href="/essensplan/src/logout.php" class="icon-menu-item" title="Abmelden">
+                <i class="fas fa-sign-out-alt"></i>
+                <span>Abmelden (<?php echo $_SESSION['username']; ?>)</span>
+            </a>
+        </div>
     <?php else: ?>
         <p>Willkommen auf dem Essensplan-Manager!</p>
         <p>Bitte <a href="/essensplan/src/login.php" class="btn btn-add"><i class="fas fa-sign-in-alt"></i> Melde dich an</a>, um fortzufahren.</p>
