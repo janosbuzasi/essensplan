bf777f10210eb426b040b9120afc5beab57ba215
janosbuzasi <janos.buzasi@bluewin.ch>
2024-09-25 19:40:33 +0200

    Update hash_password.php

--- admin/hash_password.php
+++ admin/hash_password.php
@@ -2,9 +2,14 @@
 // Beispielskript zum Hashen eines Passworts
 $password = 'antonia'; // Setze hier dein gewünschtes Passwort
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-echo 'username' $password $hashedPassword;
-<br>
+
+// Ausgabe für den ersten Benutzer
+echo 'Username: antonia, Passwort: ' . $password . ', Gehashtes Passwort: ' . $hashedPassword . '<br>';
+
+// Neuer Benutzer
 $password = 'admin'; // Setze hier dein gewünschtes Passwort
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-echo 'username' $password $hashedPassword;
+
+// Ausgabe für den zweiten Benutzer
+echo 'Username: admin, Passwort: ' . $password . ', Gehashtes Passwort: ' . $hashedPassword;
 ?>
