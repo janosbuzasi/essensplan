c52c62efcf00f7b90d09f13d6581a3730da8633a
janosbuzasi <janos.buzasi@bluewin.ch>
2024-09-25 19:23:05 +0200

    Update view_categories.php

--- src/view_categories.php
+++ src/view_categories.php
@@ -1,6 +1,6 @@
 <?php
 $title = "Mahlzeitenkategorien verwalten";
-require_once '../auth.php'; // Überprüfung der Benutzeranmeldung (auth.php einbinden)
+require_once 'auth.php'; // Überprüfung der Benutzeranmeldung (auth.php einbinden)
 require '../header.php'; // Header einfügen
 require_once '../config/db.php';
 
