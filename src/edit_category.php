ef8bcb0a6b286bbbb156ddd95b34472ecdcf19e1
janosbuzasi <janos.buzasi@bluewin.ch>
2024-09-25 19:26:05 +0200

    Update edit_category.php

--- src/edit_category.php
+++ src/edit_category.php
@@ -1,5 +1,6 @@
 <?php
 $title = "Mahlzeitenkategorie bearbeiten";
+require_once 'auth.php'; // Überprüfung der Benutzeranmeldung (auth.php einbinden)
 require '../header.php'; // Header einfügen
 require_once '../config/db.php';
 
