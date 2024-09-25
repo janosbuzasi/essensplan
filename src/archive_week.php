a1b83eac42010355e58a22eafdf1234319bd2e01
janosbuzasi <janos.buzasi@bluewin.ch>
2024-09-25 19:25:05 +0200

    Update archive_week.php

--- src/archive_week.php
+++ src/archive_week.php
@@ -1,5 +1,6 @@
 <?php
 $title = "Essensplan archivieren"; 
+require_once 'auth.php'; // Überprüfung der Benutzeranmeldung (auth.php einbinden)
 require '../header.php';  // Inkludiere den Header
 ?>
 <main>
