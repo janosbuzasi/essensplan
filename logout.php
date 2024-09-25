71d78c4fd5a526509921818063f760c8c635ee12
janosbuzasi <janos.buzasi@bluewin.ch>
2024-09-25 16:00:25 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,1 +1,24 @@
-logout.php
+<?php
+session_start(); // Start der Session
+
+// Überprüfen, ob der Benutzer eingeloggt ist
+if (isset($_SESSION['username'])) {
+    // Alle Session-Daten löschen
+    $_SESSION = array();
+    
+    // Session-Cookie löschen
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+    
+    // Session zerstören
+    session_destroy();
+}
+
+// Benutzer zurück zur Startseite leiten oder zur Anmeldeseite
+header("Location: /essensplan/index.php");
+exit;
